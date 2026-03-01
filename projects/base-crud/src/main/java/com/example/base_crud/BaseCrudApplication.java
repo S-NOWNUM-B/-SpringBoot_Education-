8b5c43743aeaaba59d9b838ef2621c9cf3fc082c
SNOWNUMB <mamayev.stas@gmail.com>
2026-03-01 17:56:49 +0500

    feat: добавлены файлы конфигурации и основной код для проекта CRUD на Spring Boot

--- projects/base-crud/src/main/java/com/example/base_crud/BaseCrudApplication.java
+++ projects/base-crud/src/main/java/com/example/base_crud/BaseCrudApplication.java
@@ -1,4 +1,4 @@
-package com.example.crud;
+package com.example.base_crud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- projects/base-crud/src/test/java/com/example/base_crud/BaseCrudApplicationTests.java
+++ projects/base-crud/src/test/java/com/example/base_crud/BaseCrudApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.crud;
+package com.example.base_crud;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
